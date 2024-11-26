1c074a1b76ba48d8e1204a8c9c45c51bbe48be0f
basdedas <567910@student.saxion.nl>
2024-11-26 12:34:09 +0100

    Initial commit

---
+++ cmake-build-debug-wsl-gcc/CMakeFiles/utf.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/utf.dir/main.cpp.o"
+  "CMakeFiles/utf.dir/main.cpp.o.d"
+  "utf"
+  "utf.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/utf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
